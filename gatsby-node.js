c299ee778248b849f7ad4d46fca5f01a24112257
Ed Siok <sioked@gmail.com>
2017-06-02 10:11:02 -0500

    Build better

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,8 +21,6 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         }
       }
     `).then(result => {
-      console.log('Got some result data');
-      console.log(result.data.allMarkdownRemark);
         if (result.errors) {
           console.log(result.errors);
           reject(result.errors);
@@ -31,13 +29,6 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         // Create blog posts pages.
         _.each(result.data.allMarkdownRemark.edges, edge => {
           console.log('Upserting page with these values: ');
-          console.log({
-            path: edge.node.fields.slug, // required
-            component: speakerTemplate,
-            context: {
-              slug: edge.node.fields.slug,
-            },
-          });
           upsertPage({
             path: edge.node.fields.slug, // required
             component: speakerTemplate,
@@ -55,15 +46,11 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
 exports.onNodeCreate = ({ node, boundActionCreators, getNode }) => {
   const { addFieldToNode } = boundActionCreators;
   if (node.internal.type === 'File') {
-    console.log('Adding file');
-    console.log(node);
     const parsedFilePath = path.parse(node.relativePath);
     const slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
     addFieldToNode({ node, fieldName: 'slug', fieldValue: slug });
   } else if (node.internal.type === 'MarkdownRemark') {
     const fileNode = getNode(node.parent);
-    console.log('Adding slug');
-    console.log(fileNode.fields.slug);
     addFieldToNode({
       node,
       fieldName: 'slug',
