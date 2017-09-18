7fa2bec709b9cf60ab37cf8a3720d7553df9bf9f
Ed Siok <sioked@gmail.com>
2017-09-18 07:42:47 -0500

    Initial commit automated image processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,24 @@ module.exports = {
         path: `${__dirname}/src`,
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        resolve: 'gatsby-remark-images',
+        options: {
+          // It's important to specify the maxWidth (in pixels) of
+          // the content container as this plugin uses this as the
+          // base for generating different widths of each image.
+          maxWidth: 590,
+          // Remove the default behavior of adding a link to each
+          // image.
+          linkImagesToOriginal: false,
+        },
+      },
+    },
+    'gatsby-plugin-sharp',
     'gatsby-transformer-json',
     'gatsby-plugin-sass',
+    'gatsby-transformer-sharp',
   ],
 };
