2276ac332a24f232b4f751eb8b44f0f16d7ffabe
Ed Siok <sioked@gmail.com>
2018-03-23 15:50:05 -0500

    IE 11 testing, add image polyfill and babel polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+require('babel-polyfill');
+const objectFitImages = require('object-fit-images');
+
+exports.onInitialClientRender = () => {
+  objectFitImages();
+};
