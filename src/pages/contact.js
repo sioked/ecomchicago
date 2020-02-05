e99a4132b402f609eee8337ab9fc2a3f586e349b
Ed Siok <sioked@gmail.com>
2020-02-04 22:06:14 -0600

    Casing on file

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContactPage from '../components/ContactPage';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 function Contact() {
   return (
